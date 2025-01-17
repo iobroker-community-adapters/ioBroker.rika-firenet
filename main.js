b063e8859f007609b96a8797746cfb7ec344368d
Martin M. <mcm57@gmx.at>
2025-01-17 17:59:20 +0100

    temp fix for crash without stoveId

--- main.js
+++ main.js
@@ -52,6 +52,10 @@ class RikaFirenet extends utils.Adapter {
 		this.log.info("config interval: " + this.config.myinterval);
 		this.log.info("config stoveid: " + this.config.mystoveid);
 
+		if (!this.config.mystoveid) {
+			console.log (`stobe id is invalid - cannot continue`);
+			this.terminate();
+		}
 		//create device
 		this.setObjectNotExists(this.config.mystoveid, {
 			type: "device",
