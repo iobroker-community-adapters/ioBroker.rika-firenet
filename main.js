e0cfc4e88c79784ebdeee015df256207dfdb9b5c
DEV2DEV_DE <github@dev2dev.de>
2025-01-20 13:26:06 +0100

    Changed wrong quote char

--- main.js
+++ main.js
@@ -8,7 +8,7 @@
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 const utils = require('@iobroker/adapter-core');
-const { default: axios } = require("axios");
+const { default: axios } = require('axios');
 //const axios = require('axios').default;
 axios.defaults.withCredentials = true;
 const { CookieJar } = require('tough-cookie');
